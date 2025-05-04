e0fbe19cffacf6113dff72927037bbb0b4d56ee1
Cememer <cememer@outlook.jp>
2025-05-04 21:50:54 +0900

    sm8350-common: Fix framework compatibility matrix for nfc
    Change-Id: I5ed962c47c977e0299083ee2c4a6ca64e179eeb4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,7 +64,8 @@ DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    device/xiaomi/sm8350-common/hidl/framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/hidl/manifest_lahaina.xml \
